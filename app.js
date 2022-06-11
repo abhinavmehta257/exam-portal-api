5b80f0d113e31f48e70c0d6ee9df9c76367f7c77
abhinavmehta257 <61967812+abhinavmehta257@users.noreply.github.com>
2022-06-11 22:50:59 +0530

    cros added

--- app.js
+++ app.js
@@ -20,8 +20,14 @@ if (!isProduction) {
     app.use(require('errorhandler')());
 }
 
-app.use(cors());
-
+// app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.urlencoded({
     extended: true
 }))
